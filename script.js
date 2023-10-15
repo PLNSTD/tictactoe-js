b59204c4e21975213835578177f21ef8ed7d1e34
Plidher <65611683+PLNSTD@users.noreply.github.com>
2023-10-15 19:24:10 -0400

    added all features, have to FIX player switch function

--- script.js
+++ script.js
@@ -14,18 +14,19 @@ const createPlayer = (name, playerSign) => {
             return oSymbol;
         }
     }
+    const getName = () => playerName;
     const getPoints = () => points;
     const addPoints = () => points++;
     const resetPoints = () => points = 0;
     const getSymbol = () => symbol;
 
-    return {playerName, getPoints, addPoints, resetPoints, getSymbol, createSymbolImg};
+    return {getName, getPoints, addPoints, resetPoints, getSymbol, createSymbolImg};
 }
 
 function tryPlayers () {
-    const player1 = createPlayer('Plidher');
+    const player1 = createPlayer('Plidher', 'x');
     console.log({
-        playerName: player1.playerName,
+        playerName: player1.getName(),
         points: player1.getPoints()
     })
     player1.addPoints();
@@ -41,17 +42,17 @@ function tryPlayers () {
 }
 
 const gameBoard = (function () {
-    const gameBoardArray = [
+    let gameBoardArray = [
         '','','',
         '','','',
         '','',''];
 
     const gameBoardDiv = document.getElementById('gameboard');
-    const restartBtn = document.getElementById('restart-button');
+    
     let cells = Array.from(gameBoardDiv.children);
-    let p1 = createPlayer('Pi','x');
-    let p2 = createPlayer('F','o');
-    let currentPlayer;
+    const p1 = createPlayer('Pi','x');
+    const p2 = createPlayer('F','o');
+    var currentPlayer = p2;
     
     function switchPlayer() {
         currentPlayer == p1 ? currentPlayer = p2 : currentPlayer = p1;
@@ -64,18 +65,36 @@ const gameBoard = (function () {
         element.appendChild(currentPlayer.createSymbolImg());
         this.removeEventListener('click', arguments.callee, false);
         element.classList.remove('box-hover');
-        checkWin(childIdx);
-        switchPlayer();
+        if(checkWin(childIdx)) {
+            displayController.winAnimation();
+            disableCells();
+        }
+        else
+            switchPlayer();
     }
 
     function restart() {
+        displayController.restartBtn.innerHTML = 'Restart';
         currentPlayer = p1;
+
+        gameBoardArray = [
+        '','','',
+        '','','',
+        '','',''];
+
         cells.forEach(element => {
             element.classList.add('box-hover');
             element.innerHTML = '';
             element.removeEventListener('click', addSign);
             element.addEventListener('click', addSign);
         });
+    }
+
+    function disableCells() {
+        cells.forEach(element => {
+            element.classList.remove('box-hover');
+            element.removeEventListener('click', addSign);
+        })
     }
 
     function checkWin(index) {
@@ -90,14 +109,17 @@ const gameBoard = (function () {
         if (columnIndex == 0) { //first column
             if (gameBoardArray[index + 1] == currentPlayer.getSymbol() && gameBoardArray[index + 2] == currentPlayer.getSymbol()) {
                 //Win
+                return true;
             }
         } else if (columnIndex == 2) { //last column
             if (gameBoardArray[index - 1] == currentPlayer.getSymbol() && gameBoardArray[index - 2] == currentPlayer.getSymbol()) {
                 //Win
+                return true;
             }
         } else { //second column
             if (gameBoardArray[index + 1] == currentPlayer.getSymbol() && gameBoardArray[index - 1] == currentPlayer.getSymbol()) {
                 //Win
+                return true;
             }
         }
     }
@@ -109,14 +131,17 @@ const gameBoard = (function () {
         if (rowIndex == 0) { //first row
             if (gameBoardArray[index + 3] == currentPlayer.getSymbol() && gameBoardArray[index + 6] == currentPlayer.getSymbol()) {
                 //Win
+                return true;
             }
         } else if (rowIndex == 2) { //last row
             if (gameBoardArray[index - 3] == currentPlayer.getSymbol() && gameBoardArray[index - 6] == currentPlayer.getSymbol()) {
                 //Win
+                return true;
             }
         } else { //second row
             if (gameBoardArray[index + 3] == currentPlayer.getSymbol() && gameBoardArray[index - 3] == currentPlayer.getSymbol()) {
                 //Win
+                return true;
             }
         }
     }
@@ -127,36 +152,71 @@ const gameBoard = (function () {
         if (rowIndex == 0 && columnIndex == 0) {
             if (gameBoardArray[index + 4] == currentPlayer.getSymbol() && gameBoardArray[index + 8] == currentPlayer.getSymbol()) {
                 //Win
-                // console.log('Win diagonal1');
+                return true;
             }
         } else if (rowIndex == 1 && columnIndex == 1) {
             if (gameBoardArray[index + 4] == currentPlayer.getSymbol() && gameBoardArray[index - 4] == currentPlayer.getSymbol()) {
                 //Win
-                //console.log('Win diagonal2');
+                return true;
             } else if (gameBoardArray[index + 2] == currentPlayer.getSymbol() && gameBoardArray[index - 2] == currentPlayer.getSymbol()) {
                 //Win
-                //console.log('Mid right to left diagonal win');
+                return true;
             }
         } else if (rowIndex == 2 && columnIndex == 2) {
             if (gameBoardArray[index - 4] == currentPlayer.getSymbol() && gameBoardArray[index - 8] == currentPlayer.getSymbol()) {
                 //Win
-                //console.log('Win diagonal3')
+                return true;
             }
         } else if (rowIndex == 0 && columnIndex == 2) {
             if (gameBoardArray[index + 2] == currentPlayer.getSymbol() && gameBoardArray[index + 4] == currentPlayer.getSymbol()) {
                 //Win
-                //console.log('Top-right diagonal win');
+                return true;
             }
         } else if (rowIndex == 2 && columnIndex == 0) {
             if (gameBoardArray[index - 2] == currentPlayer.getSymbol() && gameBoardArray[index - 4] == currentPlayer.getSymbol()) {
                 //Win
-                //console.log('Bot-left diagonal win');
+                return true;
             }
         }
     }
 
-    restartBtn.addEventListener('click', restart);
+    return {currentPlayer, restart};
+})();
 
+const displayController = (function () {
+    const restartBtn = document.getElementById('restart-button');
+    restartBtn.innerHTML = 'Play';
+    restartBtn.addEventListener('click', gameBoard.restart);
+
+    function winAnimation() {
+        console.log('WON');
+        let modalDiv = document.getElementById('myModal');
+        let congratulationPElement = document.getElementById('winner-paragraph');
+        let continuePlayingBtn = document.getElementById('continue-button');
+        let closeBtn = document.getElementById('close-button');
+        
+        console.log(gameBoard.currentPlayer.getName());
+        congratulationPElement.innerHTML = 'Congratulations ' + gameBoard.currentPlayer.getName() + ' YOU WON!';
+
+        modalDiv.style.display = 'block';
+
+        closeBtn.addEventListener('click', function () {
+            restartBtn.innerHTML = 'Play';
+            modalDiv.style.display = 'none';
+        });
+
+        window.onclick = function(e) {
+            if(e.target == modalDiv)
+                modalDiv.style.display = 'none';
+        };
+
+        continuePlayingBtn.addEventListener('click', function () {
+            modalDiv.style.display = 'none';
+            gameBoard.restart();
+        });
+    }
+
+    return {restartBtn, winAnimation};
 
 })();
 
